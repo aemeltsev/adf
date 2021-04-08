cb5219175a8a4cbeb00047e66939d5a3f62d73f1
Tony Emeltsev <aemeltsev@gmail.com>
2021-04-08 16:59:48 +0300

    chore:
    +use std::array
    +complete doc

--- adf/inc/advmath.hpp
+++ adf/inc/advmath.hpp
@@ -8,6 +8,7 @@
 #include <limits>
 #include <cfloat>
 #include <vector>
+#include <array>
 
 #include "common.hpp"
 
@@ -51,7 +52,7 @@ inline T arcsc(T p_eival, T p_eimod, int32_t p_prec)
         if(Y == 0){
             Y = std::sqrt(BT)*FLT_MIN;}
         if(std::fabs(A-B)<(A*FLT_MIN)){break;}
-        L = 2 * L;
+        L = 2 * L;vector>
         if(Y < 0.){L++;}
     }
     if(Y < 0.){L++;}
@@ -120,33 +121,43 @@ inline void ellip_funcs(T p_eival, T p_eimod, T &sn, T &cn, T &dn, int32_t p_pre
 }
 
 /**
- *  @brief
- *  @param
- *  @param
- *  @return
+ *  @brief Algorithm for calc complete elliptic integral of the first kinds using AGM process.
+ *         Start with three initialization values \f$(a_0, b_0, c_0), where
+ *         \f$(a_0 - 1, b_0 - \sqrt{1-k^2}, c_0 - k)
+ *         and on the i-th iteration cycle, the value will be as follows:
+ *         \f$(
+ *              a_i = (a_{i-1} + b_{i-1})/2,
+ *              b_i = \sqrt{a_{i-1} + b_{i-1}},
+ *              c_i = (a_{i-1} - b_{i-1})/2
+ *            )
+ *         Еhe cycle ends when \f$( a_N = b_N ) within some tolerance \f$( \varepsilon ).
+ *         And then the value of the integral will be equal to:
+ *         \f$(
+ *              E=\frac{\pi}{2a_N}
+ *            )
+ *  @param p_eimod - equal to k - the elliptic modulus
+ *  @return value of the complete elliptic integral of the first kinds
  */
 template<class T>
-T ellip_integral(T p_eimod, int32_t p_prec)
+T ellip_integral(T p_eimod)
 {
     int16_t i;
-    std::vector<T> A, B, C;
-
-    p_eimod = p_eimod*p_eimod;
+    std::array<T, MAX_TERMS> A, B, C;
 
     A[0]=1;
-    B[0]=std::sqrt(1-p_eimod);
+    B[0]=std::sqrt(1-(p_eimod*p_eimod));
     C[0]=p_eimod;
 
-    for(i=1; i<p_prec; i++)
+    for(i=1; i<MAX_TERMS; i++)
     {
         A[i]=(A[i-1]+B[i-1])/2;
         B[i]=std::sqrt(A[i-1]*B[i-1]);
         C[i]=(A[i-1]-B[i-1])/2;
 
-        if(C[i]<FLT_MIN){
+        if(C[i]<ERR_SMALL){
             break;}
     }
-    return M_PI/(2*A[i]);
+    return ADF_PI/(2*A[i]);
 }
 
 /**
