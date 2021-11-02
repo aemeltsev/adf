798360500ece399ff4bb37599f6357a797b6874c
Tony Emeltsev <aemeltsev@gmail.com>
2021-11-02 23:19:51 +0300

    tests: check approximation, type, order

--- adf/test/adftest.cpp
+++ adf/test/adftest.cpp
@@ -9,29 +9,107 @@
 
 #include "inc/adfilter.hpp"
 
-TEST(adfBttrwTst, BLPOrder)
+void tstFilterConf(const adf::FilterType& m_ftype, adf::FiltParam<double>& m_fparam)
+{
+    switch (m_ftype)
+    {
+    case adf::FilterType::LPF:
+        m_fparam.freq_passband.first = 0.1591549431;
+        m_fparam.freq_passband.second = 0.0;
+        m_fparam.freq_stopband.first = 0.3183098862;
+        m_fparam.freq_stopband.second = 0.0;
+        m_fparam.gain_passband.first = -1.0;
+        m_fparam.gain_passband.second = 0.0;
+        m_fparam.gain_stopband.first = -12.0;
+        m_fparam.gain_stopband.second = 0.0;
+        break;
+    case adf::FilterType::HPF:
+        m_fparam.freq_passband.first = 0.1591549431;
+        m_fparam.freq_passband.second = 0.0;
+        m_fparam.freq_stopband.first = 0.3183098862;
+        m_fparam.freq_stopband.second = 0.0;
+        m_fparam.gain_passband.first = -1.0;
+        m_fparam.gain_passband.second = 0.0;
+        m_fparam.gain_stopband.first = -12.0;
+        m_fparam.gain_stopband.second = 0.0;
+        break;
+    case adf::FilterType::PBF:
+        m_fparam.freq_passband.first = 0.1591549431;
+        m_fparam.freq_passband.second = 0.0;
+        m_fparam.freq_stopband.first = 0.3183098862;
+        m_fparam.freq_stopband.second = 0.0;
+        m_fparam.gain_passband.first = -1.0;
+        m_fparam.gain_passband.second = 0.0;
+        m_fparam.gain_stopband.first = -12.0;
+        m_fparam.gain_stopband.second = 0.0;
+        break;
+    case adf::FilterType::SBF:
+        m_fparam.freq_passband.first = 0.1591549431;
+        m_fparam.freq_passband.second = 0.0;
+        m_fparam.freq_stopband.first = 0.3183098862;
+        m_fparam.freq_stopband.second = 0.0;
+        m_fparam.gain_passband.first = -1.0;
+        m_fparam.gain_passband.second = 0.0;
+        m_fparam.gain_stopband.first = -12.0;
+        m_fparam.gain_stopband.second = 0.0;
+        break;
+    }
+}
+
+
+TEST(BttrwTstCase, BLPApprx)
 {
     //Arrange
-    constexpr int32_t blp_order = 3;
-    adf::FiltParam<double> blp_fparam;
-
-    blp_fparam.freq_passband.first = 0.1591549431;
-    blp_fparam.freq_passband.second = 0.0;
-    blp_fparam.freq_stopband.first = 0.3183098862;
-    blp_fparam.freq_stopband.second = 0.0;
-    blp_fparam.gain_passband.first = -1.0;
-    blp_fparam.gain_passband.second = 0.0;
-    blp_fparam.gain_stopband.first = -12.0;
-    blp_fparam.gain_stopband.second = 0.0;
-
-    adf::FilterType blp_filter = adf::FilterType::LPF;
-    adf::ApproxType blp_apprx = adf::ApproxType::BUTTER;
-    adf::CalcFilterCoefs<double> blp_coefs(blp_fparam, blp_filter, blp_apprx);
+    adf::FiltParam<double> m_fparam;
+    adf::FilterType m_ftype = adf::FilterType::LPF;
+    adf::ApproxType m_apprx = adf::ApproxType::BUTTER;
+    adf::CalcFilterCoefs<double>* m_coefs;
+    tstFilterConf(m_ftype, m_fparam);
+    m_coefs = new adf::CalcFilterCoefs<double>(m_fparam, m_ftype, m_apprx);
 
     //Act
 
     //Assert
-    ASSERT_EQ(blp_coefs.getFilterOrder(), blp_order);
+    ASSERT_EQ(m_coefs->getApproxType(), m_apprx);
 }
+
+TEST(BttrwTstSuite, BLPType)
+{
+    //Arrange
+    adf::FiltParam<double> m_fparam;
+    adf::FilterType m_ftype = adf::FilterType::LPF;
+    adf::ApproxType m_apprx = adf::ApproxType::BUTTER;
+    adf::CalcFilterCoefs<double>* m_coefs;
+    tstFilterConf(m_ftype, m_fparam);
+    m_coefs = new adf::CalcFilterCoefs<double>(m_fparam, m_ftype, m_apprx);
+
+    //Act
+
+    //Assert
+    ASSERT_EQ(m_coefs->getFilterType(), m_ftype);
+}
+
+TEST(BttrwTstSuite, BLPOrder)
+{
+    //Arrange
+    constexpr int32_t blp_order = 3;
+
+    adf::FiltParam<double> m_fparam;
+    adf::FilterType m_ftype = adf::FilterType::LPF;
+    adf::ApproxType m_apprx = adf::ApproxType::BUTTER;
+    adf::CalcFilterCoefs<double>* m_coefs;
+    tstFilterConf(m_ftype, m_fparam);
+    m_coefs = new adf::CalcFilterCoefs<double>(m_fparam, m_ftype, m_apprx);
+
+    //Act
+    m_coefs->makeFilterOrder();
+
+    //Assert
+    ASSERT_EQ(m_coefs->getFilterOrder(), blp_order);
+}
+
+
+
+
 
 #endif //ADF_TESTS_H
