67fbde8f80ecb082089d06925f2565240f065108
Tony Emeltsev <aemeltsev@gmail.com>
2021-11-19 17:59:22 +0300

    chore: update include headers

--- adf/inc/adfilter.hpp
+++ adf/inc/adfilter.hpp
@@ -3,5 +3,8 @@
 */
 #ifndef FDESIGN_H
 #define FDESIGN_H
-#include "genfilter.hpp"
+//#include "genfilter.hpp"
+#include "anfilter.hpp"
+#include "iirfilter.hpp"
+#include "fresp.hpp"
 #endif // FDESIGN_H
