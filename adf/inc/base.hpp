fe016fed5a31f1aa88b97920ba0cc7fa929f2e4b
Tony Emeltsev <aemeltsev@gmail.com>
2022-02-15 20:44:16 +0300

    add: include test heders

--- adf/inc/base.hpp
+++ adf/inc/base.hpp
@@ -1,3 +1,6 @@
+/**
+  * License text...
+*/
 #ifndef COMMON_H
 #define COMMON_H
 
---
+++ adf/test/polytest.hpp
@@ -0,0 +1,53 @@
+#ifndef POLY_TESTS_H
+#define POLY_TESTS_H
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <ctime>
+#include <cmath>
+#include <gtest/gtest.h>
+
+#include "inc/polynomial.hpp"
+
+TEST(PolySuite, AddOperator)
+{
+    adf::polynomial<int> poly{1, 2, 3, 4};
+}
+
+TEST(PolySuite, SubOperator)
+{
+
+}
+
+TEST(PolySuite, Multoperator)
+{
+
+}
+
+TEST(PolySuite, DivOperator)
+{
+
+}
+
+TEST(PolySuite, RemOperator)
+{
+
+}
+
+TEST(PolySuite, Equal)
+{
+
+}
+
+TEST(PolySuite, NotEqual)
+{
+
+}
+
+TEST(PolySuite, Index)
+{
+
+}
+
+#endif //POLY_TESTS_H
