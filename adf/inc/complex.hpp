6ef0f2d816bac81334fa90f15eb7bac7599c063e
Tony Emeltsev <aemeltsev@gmail.com>
2022-02-18 21:23:37 +0300

    chore: add friend class

--- adf/inc/complex.hpp
+++ adf/inc/complex.hpp
@@ -19,7 +19,7 @@ class complex
 public:
     //ctors
     complex() noexcept {m_re=m_im=0;}
-    explicit complex(T re, T im=0) noexcept
+    complex(T re, T im=0) noexcept
         :m_re(re)
         ,m_im(im)
     {}
@@ -106,56 +106,8 @@ public:
      */
     template<typename U>
     friend std::ostream &operator<<(std::ostream &p_out, const complex<U> &p_val);
+    template<T> friend class FFT;
 };
-
-/**
- *  @brief - square root of complex number
- *  @param - unique pointer to complex value
- *  @return - returns the square root of complex number
- */
-template<typename T>
-inline complex<T> sqrt(complex<T>& p_val)
-{
-    T real = std::sqrt(p_val.mag()) * std::cos(p_val.arg()/2.);
-    T imag = std::sqrt(p_val.mag()) * std::sin(p_val.arg()/2.);
-    return complex<T>(real, imag);
-}
-
-/**
- *  @brief - factors quadratic equation with cmplx coefficients
- *  @param - a*x^2
- *  @param - b*x
- *  @param - c
- *  @return - std::pairs with complex roots
- */
-template<typename T>
-inline std::pair<complex<T>, complex<T>> quadr(
-        complex<T>& p_a,
-        complex<T>& p_b,
-        complex<T>& p_c)
-{
-    complex<T> a2_var,
-               ac4_tmp, ac4_var,
-               sq1_tmp, sq2_tmp, sq_var,
-               fr_root, sc_root,
-               pb_tmp = p_b;
-    complex<T> fr_const(2., 0.);
-    complex<T> sc_const(4., 0.);
-
-    a2_var = fr_const * p_a; /**< 2*a */
-
-    ac4_tmp = sc_const * p_a;
-    ac4_var = ac4_tmp * p_c; /**< 4*a*c */
-
-    sq1_tmp = p_b * p_b;
-    sq2_tmp = sq1_tmp - ac4_var;
-    sq_var = adf::sqrt(sq2_tmp); /**< sqrt(b*b - 4*a*c) */
-
-    pb_tmp = -pb_tmp;
-    fr_root = pb_tmp + sq_var; /**< for first root */
-    sc_root = pb_tmp - sq_var; /**< for second root */
-    return std::make_pair(fr_root/a2_var, sc_root/a2_var);
-}
 
 template<typename T>
 std::ostream &operator<<(std::ostream &p_out, const complex<T> &p_val)
