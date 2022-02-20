8714a7cd0773bd77e8af03032043e6c6af17e75d
Tony Emeltsev <aemeltsev@gmail.com>
2022-02-20 21:31:27 +0300

    chore: polynomial multiply implementation

--- adf/inc/polynomial.hpp
+++ adf/inc/polynomial.hpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
-#include <utility>
 #include <algorithm>
 #include <initializer_list>
 
@@ -39,6 +38,51 @@ class polynomial
     void _reverse(std::vector<T>& other)
     {
         std::reverse(other.begin(), other.end());
+    }
+
+    void _padding(polynomial<T>& lhs, polynomial<T>& rhs)
+    {
+        auto add_zero = [&](std::size_t len, std::vector<T> v) -> void
+        {
+            while(v.size() < len){
+                v.push_back(T(0));
+            }
+        };
+
+        auto lhs_size = lhs.size();
+        auto rhs_size = rhs.size();
+
+        if((lhs_size == rhs_size) && lhs_size == 1) {return;}
+
+        else if((lhs_size == rhs_size) && (lhs_size % 2 == 0)) {return;}
+
+        else{
+            auto max_size = std::max(lhs_size, rhs_size);
+
+            if(max_size == 2){
+                if(lhs_size > rhs_size)
+                    add_zero(2, rhs.m_data);
+                else
+                    add_zero(2, lhs.m_data);
+            }
+            else if(max_size == 3 || max_size == 4){
+                add_zero(4, lhs.m_data);
+                add_zero(4, rhs.m_data);
+            }
+            else{
+                add_zero(max_size, lhs.m_data);
+                add_zero(max_size, rhs.m_data);
+            }
+        }
+    }
+
+    void _shift_pow10(std::size_t n, std::vector<T>& v)
+    {
+        std::size_t i = 0;
+        while(i < n){
+            v.insert(v.begin(), 0);
+            ++i;
+        }
     }
 
 public:
@@ -151,6 +195,16 @@ public:
         return tmp.m_data;
     }
 
+    void padding(polynomial<T>& other)
+    {
+        _padding(*this, other);
+    }
+
+    void shift_pow10(std::size_t n)
+    {
+        _shift_pow10(n, this->m_data);
+    }
+
     /**
      * @brief overloaded operators
      */
@@ -158,11 +212,11 @@ public:
     polynomial<T> operator+() const { return *this; }
     polynomial<T> operator-() const;
 
-    polynomial<T> operator+(const polynomial<T> &other) const;
-    polynomial<T> operator-(polynomial<T>& other) const;
-    polynomial<T> operator*(polynomial<T>& other) const;
-    polynomial<T> operator/(polynomial<T>& other) const;
-    polynomial<T> operator%(polynomial<T>& other) const;
+    polynomial<T> operator+(const polynomial<T>& other) const;
+    polynomial<T> operator-(const polynomial<T>& other) const;
+    polynomial<T> operator*(const polynomial<T>& other) const;
+    polynomial<T> operator/(const polynomial<T>& other) const;
+    polynomial<T> operator%(const polynomial<T>& other) const;
 
     polynomial<T> operator^(polynomial<T>& other) const;
 
@@ -190,6 +244,15 @@ public:
      */
     template<typename U>
     friend std::ostream &operator<<(std::ostream &p_out, const polynomial<U> &p_val);
+
+private:
+    template<typename U>
+    friend polynomial<U> karatsuba(polynomial<U>& rhs,
+                                   polynomial<U>& lhs);
+
+    template<typename U>
+    friend polynomial<U> newton(const polynomial<U>& rhs,
+                                const polynomial<U>& lhs);
 };
 
 template<typename U>
@@ -205,10 +268,95 @@ std::ostream &operator<<(std::ostream &p_out, const polynomial<U> &p_val)
     return p_out;
 }
 
-template<typename U> bool operator==(polynomial<U> &p_fr, polynomial<U> &p_sc);
-template<typename U> bool operator!=(polynomial<U> &p_fr, polynomial<U> &p_sc);
-template<typename U> bool operator==(const polynomial<U> &p_fr, const polynomial<U> &p_sc);
-template<typename U> bool operator!=(const polynomial<U> &p_fr, const polynomial<U> &p_sc);
+template<typename U>
+polynomial<U> karatsuba(polynomial<U>& lhs, polynomial<U>& rhs)
+{
+    auto lhs_size = lhs.size();
+    auto rhs_size = rhs.size();
+
+    if(lhs_size == 1 && rhs_size == 1)
+    {
+        polynomial<U> ans(lhs_size);
+        U m = lhs[0] * rhs[0];
+        ans.m_data[0] = m;
+        return ans;
+    }
+    else{
+
+        lhs.padding(rhs);
+        auto len = lhs.size();
+        std::size_t half = len / 2;
+
+        polynomial<U> result(len * 2);
+
+        /* Split the digit sequences in the middle. */
+        polynomial<U> a_lhs(half);
+        polynomial<U> a_rhs(half);
+
+        for(std::size_t i = 0; i < half; ++i)
+        {
+            a_lhs.m_data[i] = lhs.m_data[i];
+            a_rhs.m_data[i] = rhs.m_data[i];
+        }
+
+        polynomial<U> b_lhs(half);
+        polynomial<U> b_rhs(half);
+
+        for(std::size_t i = half; i < len; ++i)
+        {
+            b_lhs.m_data[i - half] = lhs.m_data[i];
+            b_rhs.m_data[i - half] = rhs.m_data[i];
+        }
+
+        /* 3 recursive calls made to numbers approximately half the size. */
+        polynomial<U> c_2 = karatsuba(b_lhs, b_rhs);
+        polynomial<U> c_0 = karatsuba(a_lhs, a_rhs);
+
+        polynomial<U> a_mid = b_lhs + a_lhs;
+        polynomial<U> b_mid = b_rhs + a_rhs;
+
+        polynomial<U> p_1 = karatsuba(a_mid, b_mid);
+        polynomial<U> tmp = c_2 + c_0;
+        polynomial<U> c_1 = p_1 - tmp;
+
+        if((len % 2) != 0){
+            c_2.shift_pow10(len - 1);
+        }
+        else{
+            c_2.shift_pow10(len);
+        }
+        c_1.shift_pow10(half);
+
+        polynomial<U> c_21 = c_2 + c_1;
+        c_21.normalize();
+        result = c_21 + c_0;
+        result.normalize();
+    }
+}
+
+template<typename U>
+polynomial<U> newton(const polynomial<U>& rhs,
+                     const polynomial<U>& lhs)
+{
+
+}
+
+template<typename U> bool operator==(const polynomial<U> &p_fr, const polynomial<U> &p_sc)
+{
+    if(p_fr.size() != p_sc.size())
+        return false;
+    for(std::size_t i = 0; i < p_fr.size(); ++i)
+    {
+        if(p_fr[i] != p_sc[i])
+            return false;
+    }
+    return true;
+}
+
+template<typename U> bool operator!=(const polynomial<U> &p_fr, const polynomial<U> &p_sc)
+{
+    return p_fr == p_sc;
+}
 
 /**
  * @brief implementation
@@ -240,25 +388,23 @@ polynomial<T> polynomial<T>::operator+(const polynomial<T>& other) const
 }
 
 template<typename T>
-polynomial<T> polynomial<T>::operator-(polynomial<T>& other) const
+polynomial<T> polynomial<T>::operator-(const polynomial<T>& other) const
+{return (*this) + (-other);}
+
+template<typename T>
+polynomial<T> polynomial<T>::operator*(const polynomial<T> &other) const
 {
 
 }
 
 template<typename T>
-polynomial<T> polynomial<T>::operator*(polynomial<T>& other) const
+polynomial<T> polynomial<T>::operator/(const polynomial<T> &other) const
 {
 
 }
 
 template<typename T>
-polynomial<T> polynomial<T>::operator/(polynomial<T>& other) const
-{
-
-}
-
-template<typename T>
-polynomial<T> polynomial<T>::operator%(polynomial<T>& other) const
+polynomial<T> polynomial<T>::operator%(const polynomial<T>& other) const
 {
 
 }
@@ -301,15 +447,11 @@ polynomial<T> polynomial<T>::operator%(const T& data) const
 
 template<typename T>
 polynomial<T>& polynomial<T>::operator+=(const polynomial<T>& other)
-{
-    return *this = *this + other;
-}
+{ return *this = *this + other; }
 
 template<typename T>
 polynomial<T>& polynomial<T>::operator-=(const polynomial<T>& other)
-{
-
-}
+{ return *this = *this - other; }
 
 template<typename T>
 polynomial<T>& polynomial<T>::operator*=(const polynomial<T>& other)
